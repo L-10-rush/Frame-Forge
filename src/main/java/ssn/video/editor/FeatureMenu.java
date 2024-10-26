686414c54b08a19700aed4f5282f5b0372773c1c
FlashGrey3000 <aaftabkhan2005@gmail.com>
2024-10-26 12:57:09 +0530

    little changes

---
+++ src/main/java/ssn/video/editor/FeatureMenu.java
@@ -0,0 +1,21 @@
+package ssn.video.editor;
+
+import java.util.ArrayList;
+
+class Features{
+    ArrayList<String> featureList = new ArrayList<>();
+
+    Features(){
+        featureList.add("Gaussian Blur");
+        featureList.add("Trim Video");
+        featureList.add("Gaussian Blur");
+        featureList.add("Gaussian Blur");
+        featureList.add("Gaussian Blur");
+    }
+}
+
+public class FeatureMenu {
+    public static void main(String[] args){
+        
+    }
+}
--- src/main/java/ssn/video/editor/rapparappa.java
+++
@@ -1,7 +0,0 @@
-package ssn.video.editor;
-
-public class rapparappa {
-    public static void main(String args[]){
-        System.out.println("Helloooooo heheheheh");
-    }
-}
